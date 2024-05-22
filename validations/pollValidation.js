317aa422f647ed14880eb22e55395ac8a48db51a
serhatunver <unverserhat2@gmail.com>
2024-05-23 00:49:20 +0300

    fix: limit length of the options array

--- validations/pollValidation.js
+++ validations/pollValidation.js
@@ -4,11 +4,14 @@ export default {
   createPoll: {
     [Segments.BODY]: Joi.object({
       question: Joi.string().required().min(6).max(120),
-      options: Joi.array().items(
-        Joi.object({
-          value: Joi.string().min(2).max(24).required()
-        })
-      )
+      options: Joi.array()
+        .min(2)
+        .max(10)
+        .items(
+          Joi.object({
+            value: Joi.string().min(2).max(24).required()
+          })
+        )
     })
   },
   getPoll: {
